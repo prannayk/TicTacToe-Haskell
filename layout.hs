db6a840c605533a0d5571151d54189ab151909f5
Prannay Khosla <prannay.khosla@gmail.com>
2016-02-25 05:59:29 +0530

    Lazy computation plus operations to ease evaluations

--- layout.hs
+++ layout.hs
@@ -1,9 +1,57 @@
+import System.IO
+
+infixr 5 .+
+infixr 5 ..+
+infixr 5 .*
+infixr 5 ..*
+infixr 5 ./
+infixr 5 ../
+
 data Cell = O | X | Empty deriving (Show,Eq)
 type Row a = [a]
 type GameState = Row (Row Cell)
+type Won = Cell
+type GamePlay = (Cell,Won,GameState)
 
-initiliazeEmpty :: GameState
-initiliazeEmpty = [initiliazeERow,initiliazeERow,initiliazeERow]
+data Tree a = EmptyTree | Node a (Row (Tree a)) deriving (Eq, Show)
+type GameTree = Tree GamePlay
+
+(.+) :: Cell -> Cell -> Cell
+Empty .+ ys = ys
+ys .+ Empty = ys
+x .+ y = x
+
+inv :: Cell -> Cell
+inv O = X
+inv X = O
+
+initiliazeGames :: [GameState]
+initiliazeGames = [[[X,Empty,Empty],[Empty,Empty,Empty],[Empty,Empty,Empty]],[[Empty,X,Empty],[Empty,Empty,Empty],[Empty,Empty,Empty]],[[Empty,Empty,X],[Empty,Empty,Empty],[Empty,Empty,Empty]],[[Empty,Empty,Empty],[X,Empty,Empty],[Empty,Empty,Empty]],[[Empty,Empty,Empty],[Empty,X,Empty],[Empty,Empty,Empty]],[[Empty,Empty,Empty],[Empty,Empty,X],[Empty,Empty,Empty]],[[Empty,Empty,Empty],[Empty,Empty,Empty],[X,Empty,Empty]],[[Empty,Empty,Empty],[Empty,Empty,Empty],[Empty,X,Empty]],[[Empty,Empty,Empty],[Empty,Empty,Empty],[Empty,Empty,X]]]
+
+remove :: GameState -> [GameState] -> [GameState]
+remove game games = filter (\x -> (x /= game)) games
+
+(.*) :: Cell -> Cell -> Cell
+O .* X = O
+X .* X = X
+_ .* Empty = Empty
+
+(./) :: Cell -> Cell -> Cell
+O ./ O = X
+X ./ X = X
+Empty ./ _ = Empty
+
+(..+) :: GameState -> GameState -> GameState
+game ..+ move = zipWith (\x y -> (zipWith (.+) x y)) game move
+
+(..*) :: Cell -> GameState -> GameState
+cell ..* game = map (map (cell .* )) game
+
+(../) :: Cell -> GameState -> GameState
+cell ../ game = map (map ( ./ cell)) game
+
+initializeEmpty :: GameState
+initializeEmpty = [initiliazeERow,initiliazeERow,initiliazeERow]
 
 initiliazeERow :: Row Cell
 initiliazeERow = [Empty,Empty,Empty]
@@ -18,11 +66,11 @@ rowDone row
 	| otherwise = Empty
 
 column1 :: GameState -> Row Cell
-column1 game = foldr (\(x:y:z:[]) acc  -> x:acc) (initiliazeRow) game
+column1 ((x:_):(y:_):(z:_):_) = x:y:z:(initiliazeRow)
 column2 :: GameState -> Row Cell
-column2 game = foldr (\(x:y:z:[]) acc -> y:acc) (initiliazeRow) game
+column2 ((_:x:_):(_:y:_):(_:z:_):_) = x:y:z:(initiliazeRow)
 column3 :: GameState -> Row Cell
-column3 game = foldr (\(x:y:z:[]) acc -> z:acc) (initiliazeRow) game
+column3 ((_:_:x:_):(_:_:y:_):(_:_:z:_):_) = x:y:z:(initiliazeRow)
 diag1 :: GameState -> Row Cell
 diag1 ((x:_):(_:y:_):(_:_:z:[]):[]) = x:y:z:(initiliazeRow)
 diag2 :: GameState -> Row Cell
@@ -30,3 +78,9 @@ diag2 ((_:_:x:_):(_:y:_):(z:_):_) = x:y:z:(initiliazeRow)
 
 won :: GameState -> Cell
 won game = foldl (\acc x -> let res = rowDone x in if(res/=Empty) then res else acc) Empty game
+
+showGame :: GameState -> IO ()
+showGame [] = do return ()
+showGame (x:xs) = do
+	putStrLn $ foldl (\acc x -> if(x == Empty) then acc++"  " else acc++" "++((show)x )) "" x
+	showGame xs
