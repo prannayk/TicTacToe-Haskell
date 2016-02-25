2fc6f298efdf3ba233741492c21d92ae0fff909e
Prannay Khosla <prannay.khosla@gmail.com>
2016-02-26 00:43:25 +0530

    Two player completed

--- layout.hs
+++ layout.hs
@@ -1,4 +1,5 @@
 import System.IO
+import System.Process
 
 infixr 5 .+
 infixr 5 ..+
@@ -7,11 +8,10 @@ infixr 5 ..*
 infixr 5 ./
 infixr 5 ../
 
-data Cell = O | X | Empty deriving (Show,Eq)
+data Cell = O | X | Empty | Q | W | E | A | S | D | Z | C | V deriving (Show,Eq)
 type Row a = [a]
 type GameState = Row (Row Cell)
-type Won = Cell
-type GamePlay = (Cell,Won,GameState)
+type GamePlay = (Cell,Cell,GameState)
 
 data Tree a = EmptyTree | Node a (Row (Tree a)) deriving (Eq, Show)
 type GameTree = Tree GamePlay
@@ -19,7 +19,8 @@ type GameTree = Tree GamePlay
 (.+) :: Cell -> Cell -> Cell
 Empty .+ ys = ys
 ys .+ Empty = ys
-x .+ y = x
+X .+ _ = X
+O .+ _ = O
 
 inv :: Cell -> Cell
 inv O = X
@@ -32,8 +33,8 @@ remove :: GameState -> [GameState] -> [GameState]
 remove game games = filter (\x -> (x /= game)) games
 
 (.*) :: Cell -> Cell -> Cell
-O .* X = O
-X .* X = X
+Empty .* X = Empty
+any .* X = any
 _ .* Empty = Empty
 
 (./) :: Cell -> Cell -> Cell
@@ -77,10 +78,45 @@ diag2 :: GameState -> Row Cell
 diag2 ((_:_:x:_):(_:y:_):(z:_):_) = x:y:z:(initiliazeRow)
 
 won :: GameState -> Cell
-won game = foldl (\acc x -> let res = rowDone x in if(res/=Empty) then res else acc) Empty game
+won game = foldl (\acc x -> let res = rowDone x in if(res/=Empty) then res else acc) Empty ((map (\x -> x game) [column1,column2,column3,diag1,diag2]) ++ game)
+
+numberedState :: Row GameState -> GameState
+numberedState gamesLeft = foldl (\acc x -> x ..+ acc) (initializeEmpty) (zipWith (..*) [Q,W,E,A,S,D,Z,C,V] gamesLeft)
 
 showGame :: GameState -> IO ()
 showGame [] = do return ()
 showGame (x:xs) = do
-	putStrLn $ foldl (\acc x -> if(x == Empty) then acc++"  " else acc++" "++((show)x )) "" x
+	putStrLn $ foldl (\acc x -> acc++" "++(show)x) "" x
 	showGame xs
+	return ()
+
+anal :: String -> Int
+anal input
+ | input == "Q" = 1
+ | input == "W" = 2
+ | input == "E" = 3
+ | input == "A" = 4
+ | input == "S" = 5
+ | input == "D" = 6
+ | input == "Z" = 7
+ | input == "C" = 8
+ | input == "V" = 9
+
+thisMove :: Row GameState -> String -> Cell -> GameState
+thisMove gamesLeft num move = (last $ take (anal num) $ map (move ..* ) gamesLeft)
+
+playGame :: GamePlay -> Row GameState -> IO ()
+playGame (move,winner,game) gamesLeft = do
+	callCommand "clear"
+	showGame (game ..+ (numberedState gamesLeft))
+	if (winner == Empty) then do
+		num <- getLine
+		playGame ((inv move),(won (game ..+ (thisMove gamesLeft num move))),(game ..+ (thisMove gamesLeft num move))) (filter (\x -> x /= (thisMove gamesLeft num move)) gamesLeft)
+		return ()
+	else 
+		putStrLn $ (show)winner ++ " has Won the game!"
+
+main :: IO ()
+main = do
+	playGame (O,Empty,(initializeEmpty)) initiliazeGames
+	return ()
